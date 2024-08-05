f5545339de9217ec701105858208c5480d79553a
asutraye <asutraye@ASUTRAYE-T490.in.oracle.com>
2024-08-05 06:46:05 +0530

    user applicaton through shell script

---
+++ redis.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Installing RedisDB through this shell script
+
+ID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGSFILE="/tmp/$0-$TIMESTAMP.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Script is started execution at $Y $TIMESTAMP $N" &>> $LOGSFILE
+
+CHECK(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2.. $R Failed $N"
+        exit 1
+    else
+        echo -e "$2.. $G Success $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo "Error: Run the script with the root user"
+    exit  1 # if not true
+else
+    echo -e "You are a $G root $N user"
+fi
